2a25c2caed37df36e10001551c76c1e5532d52d5
divine dela <divinedela.dd@gmail.com>
2025-08-14 12:42:06 +0000

    fix: use new firebase admin config in sendNotification service

--- services/sendNotification.js
+++ services/sendNotification.js
@@ -1,94 +1,87 @@
-// Importing required modules 
-const admin = require('firebase-admin');
+// Importing required modules
 const moment = require("moment");
 
-const serviceAccount  = {
-    projectId: process.env.firebase_projectId,
-    clientEmail: process.env.firebase_client_email,
-    privateKey: process.env.firebase_private_key.replace(/\\n/g, '\n')
-}
+const { admin } = require("../config/firebaseAdmin");
+
+const serviceAccount = {
+  projectId: process.env.firebase_projectId,
+  clientEmail: process.env.firebase_client_email,
+  privateKey: process.env.firebase_private_key.replace(/\\n/g, "\n"),
+};
 
 // Importing models
 const userNotificationModel = require("../model/userNotificationModel");
 const notificationModel = require("../model/notificationModel");
 
-// Initialize Firebase Admin SDK
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
-
 // fetch all user toekn
 async function fetchAllUserToken(title, message) {
+  try {
+    const userToken = await userNotificationModel.find({});
 
-    try {
+    if (userToken.length > 0) {
+      // Save notification to database
+      await new notificationModel({
+        recipient: "User",
+        title: title,
+        message: message,
+      }).save();
 
-        const userToken = await userNotificationModel.find({});
+      // Retrieve registration token for the user
+      const registrationTokens = userToken.map(
+        (token) => token.registrationToken
+      );
 
-        if (userToken.length > 0) {
-
-            // Save notification to database
-            await new notificationModel({ recipient: "User", title: title, message: message }).save();
-
-            // Retrieve registration token for the user
-            const registrationTokens = userToken.map(token => token.registrationToken);
-
-            // Send push notification
-            await sendPushNotification(registrationTokens, title, message);
-        }
-
-    } catch (error) {
-        console.log("error fetch user registration token", error);
+      // Send push notification
+      await sendPushNotification(registrationTokens, title, message);
     }
+  } catch (error) {
+    console.log("error fetch user registration token", error);
+  }
 }
-
 
 // Firebase Push Notification
 async function sendPushNotification(registrationTokens, title, message) {
+  const promises = registrationTokens.map((token) => {
+    const messagePayload = {
+      notification: {
+        title: title,
+        body: message,
+      },
+      token: token,
+    };
 
-    const promises = registrationTokens.map(token => {
+    return admin.messaging().send(messagePayload);
+  });
 
-        const messagePayload = {
-            notification: {
-                title: title,
-                body: message,
-            },
-            token: token,
-        };
+  try {
+    // Send all notifications and wait for them to complete
+    const responses = await Promise.all(promises);
 
-        return admin.messaging().send(messagePayload);
-    });
+    console.log("Notifications sent successfully:", responses);
 
-    try {
-        // Send all notifications and wait for them to complete
-        const responses = await Promise.all(promises);
-
-        console.log('Notifications sent successfully:', responses);
-
-        // // Log failures
-        // responses.forEach((resp, index) => {
-        //     if (!resp.success) {
-        //         console.error(`Failed to send notification to token ${registrationTokens[index]}:`, resp.error);
-        //     }
-        // });
-
-    } catch (error) {
-        console.error('Error sending notifications:', error);
-    }
+    // // Log failures
+    // responses.forEach((resp, index) => {
+    //     if (!resp.success) {
+    //         console.error(`Failed to send notification to token ${registrationTokens[index]}:`, resp.error);
+    //     }
+    // });
+  } catch (error) {
+    console.error("Error sending notifications:", error);
+  }
 }
-
 
 // Function to send notification to a admin
 async function sendAdminNotification(title, message) {
-
-    try {
-
-        // Save admin notification 
-        await new notificationModel({ title, recipient: "Admin", message }).save();
-
-    } catch (error) {
-        console.error("Error sending admin notification:", error);
-    }
+  try {
+    // Save admin notification
+    await new notificationModel({ title, recipient: "Admin", message }).save();
+  } catch (error) {
+    console.error("Error sending admin notification:", error);
+  }
 }
 
-
-module.exports = { fetchAllUserToken, sendPushNotification, sendAdminNotification };
+module.exports = {
+  fetchAllUserToken,
+  sendPushNotification,
+  sendAdminNotification,
+};
